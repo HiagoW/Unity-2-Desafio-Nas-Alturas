14e99d7a09adc980011d962d3d19d0cb6a56cfef
Hiago W. Petris <hiago.petris@gmail.com>
2023-03-26 18:02:45 -0300

    first commit

---
+++ Assets/Scripts/Obstaculo.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstaculo : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Translate(Vector3.left * 0.1f);
+    }
+}
